60e7ac15dd4e6cc18a2c8e07daee32ff4513d1ce
mbayedeweb <ibzembaye@gmail.com>
2024-08-19 15:11:03 +0000

    second commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import { createApp } from "vue";
 // import "./style.css";
 import App from "./App.vue";
